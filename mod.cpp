55d069fd8ddd21af38a83c8c831eb56fee8af490
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2025-05-29 18:50:32 +0200

    Mod Config Set up

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+name = "AComS";
+picture = "";
+actionName = "Website";
+action = "https://github.com/Goaty1208";
+description = "AI Command System";
+logo = "";
+logoOver = "";
+tooltip = "AComS";
+tooltipOwned = "AComS";
+overview = "Batallion level simulation";
+author = "Goaty1208";
+overviewPicture = "";
+overviewText = "overviewText";
+overviewFootnote = "<br /><br /><t color='#999999'>This content is under the APL-SA License.<br />Press <t /><t color='#19d3ff'>Left Shift + P<t /><t color='#999999'> to open the store page for more information.<t />";
