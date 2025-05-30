38f01108116fb61ed7c8d2e080b8de92d6507ebd
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2025-05-30 15:31:52 +0200

    CBA Path Macros

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -42,7 +42,7 @@ class CfgVehicles {
 		// Standard object definitions:
 		scope = 2;										// Editor visibility; 2 will show it in the menu, 1 will hide it.
 		displayName = "Core";				// Name displayed in the menu
-		icon = "\z\acoms\addons\main\data\acoms_BW_CA.paa";	// Map icon. Delete this entry to use the default icon.
+		icon = QUOTE(PATHTOF(data\acoms_BW_CA.paa));	// Map icon. Delete this entry to use the default icon.
 		category = "GOATY_AComS_Category";
 
 		function = "";	// Name of function triggered once conditions are met
@@ -118,7 +118,7 @@ class CfgVehicles {
 			class BluforUnit {
 				description = "Short description";
 				displayName = "Any BLUFOR unit";	  // Custom name
-				icon = "\z\acoms\addons\main\data\acoms_BW_CA.paa";  // Custom icon (can be file path or CfgVehicleIcons entry)
+				icon = QUOTE(PATHTOF(data\acoms_BW_CA.paa));  // Custom icon (can be file path or CfgVehicleIcons entry)
 				side = 1;							  // Custom side (determines icon color)
 			};
 		};
